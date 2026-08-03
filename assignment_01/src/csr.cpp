75827d5d7d228668e2917f59400cd704d97912f3
Arshdeep2026AIM <2026aim1004iitrpr.ac.in>
2026-08-03 22:50:22 +0530

    Included header files, implmented bfs and set up dfs to be completed by duo

--- assignment_01/src/csr.cpp
+++ assignment_01/src/csr.cpp
@@ -1,94 +1,78 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include "csr.h"
+
 using namespace std;
 
-struct CSRGraph {
-    int numVertices = 0;
-    int numEdges = 0;
-    int sourceVertex = 0;
+void Csr::convert(string path) {
+    ifstream file(path);
+    if (!file.is_open()) return;
 
-    vector<int> rowPtr;
-    vector<int> colIdx;
-    vector<int> values;
-};
+    csrGraph.rowPtr.clear();
+    csrGraph.colIdx.clear();
+    csrGraph.values.clear();
 
-class CsrConversion {
-    public:
-        CSRGraph csrGraph;
+    int index = 0;
+    file >> csrGraph.numVertices >> csrGraph.numEdges;
+    csrGraph.rowPtr.reserve(csrGraph.numVertices + 1);
+    csrGraph.colIdx.reserve(2 * csrGraph.numEdges);
+    csrGraph.values.reserve(2 * csrGraph.numEdges);
 
-        void convert(string path) {
-            ifstream file(path);
-            if (!file.is_open()) return;
+    for (int vertice = 0; vertice < csrGraph.numVertices; vertice++) {
+        int v, degree;
+        file >> v >> degree;
+        csrGraph.rowPtr.push_back(index);
 
-            csrGraph.rowPtr.clear();
-            csrGraph.colIdx.clear();
-            csrGraph.values.clear();
-
-            int index = 0;
-            file >> csrGraph.numVertices >> csrGraph.numEdges;
-            csrGraph.rowPtr.reserve(csrGraph.numVertices + 1);
-            csrGraph.colIdx.reserve(2 * csrGraph.numEdges);
-            csrGraph.values.reserve(2 * csrGraph.numEdges);
-
-            for (int vertice = 0; vertice < csrGraph.numVertices; vertice++) {
-                
-                int v, degree;
-                file >> v >> degree;
-                csrGraph.rowPtr.push_back(index);
-
-                for (int i = 0; i < degree; i++) {
-                    int neighbor;
-                    file >> neighbor;
-                    csrGraph.colIdx.push_back(neighbor);
-                    index++;
-                }
-            }
-            csrGraph.rowPtr.push_back(index);
-
-            string extra;
-            file >> extra;
-            file >> csrGraph.sourceVertex;
-
-            file.close();
-    
+        for (int i = 0; i < degree; i++) {
+            int neighbor;
+            file >> neighbor;
+            csrGraph.colIdx.push_back(neighbor);
+            index++;
         }
+    }
+    csrGraph.rowPtr.push_back(index);
 
-        void convert(string path, bool isWeighted) {
-            ifstream file(path);
-            if (!file.is_open()) return;
+    string extra;
+    file >> extra;
+    file >> csrGraph.sourceVertex;
 
-            csrGraph.rowPtr.clear();
-            csrGraph.colIdx.clear();
-            csrGraph.values.clear();
+    file.close();
+}
 
-            int index = 0;
-            file >> csrGraph.numVertices >> csrGraph.numEdges;
-            csrGraph.rowPtr.reserve(csrGraph.numVertices + 1);
-            csrGraph.colIdx.reserve(2 * csrGraph.numEdges);
-            csrGraph.values.reserve(2 * csrGraph.numEdges);
+void Csr::convert(string path, bool isWeighted) {
+    ifstream file(path);
+    if (!file.is_open()) return;
 
-            for (int vertice = 0; vertice < csrGraph.numVertices; vertice++) {
-                
-                int v, degree;
-                file >> v >> degree;
-                csrGraph.rowPtr.push_back(index);
-                
-                for (int i = 0; i < degree; i++) {
-                    int neighbor, weight;
-                    file >> neighbor;
-                    file >> weight;
-                    csrGraph.colIdx.push_back(neighbor);
-                    csrGraph.values.push_back(weight);
-                    index++;
-                }
-            }
+    csrGraph.rowPtr.clear();
+    csrGraph.colIdx.clear();
+    csrGraph.values.clear();
 
-            string extra;
-            file >> extra;
-            file >> csrGraph.sourceVertex;
+    int index = 0;
+    file >> csrGraph.numVertices >> csrGraph.numEdges;
+    csrGraph.rowPtr.reserve(csrGraph.numVertices + 1);
+    csrGraph.colIdx.reserve(2 * csrGraph.numEdges);
+    csrGraph.values.reserve(2 * csrGraph.numEdges);
 
-            file.close();
+    for (int vertice = 0; vertice < csrGraph.numVertices; vertice++) {
+        int v, degree;
+        file >> v >> degree;
+        csrGraph.rowPtr.push_back(index);
+
+        for (int i = 0; i < degree; i++) {
+            int neighbor, weight;
+            file >> neighbor;
+            file >> weight;
+            csrGraph.colIdx.push_back(neighbor);
+            csrGraph.values.push_back(weight);
+            index++;
         }
+    }
+    csrGraph.rowPtr.push_back(index);
 
-};
+    string extra;
+    file >> extra;
+    file >> csrGraph.sourceVertex;
+
+    file.close();
+}
